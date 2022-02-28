47b94da0739cec5793984ca8e02615969cbf8bf3
Ingo Ruhnke <grumbel@gmail.com>
2022-02-28 19:57:36 +0100

    Remove superfluous nix input from Nix flake

--- flake.nix
+++ flake.nix
@@ -1,13 +1,12 @@
 {
-  description = "A collection of point, size and rect classes for C++";
+  description = "XDG Base Directory Specification in C++11";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
-    nix.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nix, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -19,8 +18,6 @@
             src = nixpkgs.lib.cleanSource ./.;
             nativeBuildInputs = [
               pkgs.cmake
-              pkgs.ninja
-              pkgs.gcc
             ];
            };
         };

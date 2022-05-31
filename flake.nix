e2e50c0c78abfaf4647d9640638e5bed0fa7250d
Ingo Ruhnke <grumbel@gmail.com>
2022-05-31 15:30:21 +0200

    Update flake.nix to nixos-22.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "XDG Base Directory Specification in C++11";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
